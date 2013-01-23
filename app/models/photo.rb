20bf0c8017d8786556dcc7998c2499aa62241c50
Vincent Samaco <vsamaco@gmail.com>
2013-01-23 01:20:36 -0800

    Re-enable uploader photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :image, :image_cache, :image_url, :remove_image, :name
 
-  # mount_uploader :image, PhotoUploader
+  mount_uploader :image, PhotoUploader
 end
