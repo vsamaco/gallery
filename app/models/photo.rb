8d61bbd93050ae961af3d0584dccdf0c652ee424
Vincent Samaco <vsamaco@gmail.com>
2013-01-22 23:31:38 -0800

    Setup Cloudinary Uploader for photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :description, :image, :name
+  attr_accessible :description, :image, :image_cache, :image_url, :remove_image, :name
+
+  mount_uploader :image, PhotoUploader
 end
